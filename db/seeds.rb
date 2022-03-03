55519ddcb764afb91f506f6d16a15afede7f9065
laure reina <laure.reina@gmail.com>
2022-03-03 15:17:07 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Candle.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+vanilla = Category.create!(name: 'vanilla')
+strawberry = Category.create!(name: 'strawberry')
+
+puts 'Creating teddies...'
+Candle.create!(name: 'Madagascan Vanilla', category: vanilla)
+
+Candle.create!(name: 'Wild Jungle', category: vanilla)
+Candle.create!(name: 'Blush Living', category: strawberry)
+puts 'Finished!'
