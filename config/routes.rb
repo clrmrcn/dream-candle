bc58965f3dc630fabb84fc69b14fdd01f8d994ed
laure reina <laure.reina@gmail.com>
2022-03-03 16:40:05 +0100

    stripe

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :candles, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: :new
+  end
 end
